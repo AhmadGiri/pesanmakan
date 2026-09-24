b3988e0b00d48f8c50e6c1c41e056785341d7114
Gir <ahmad.giri195@smk.belajar.id>
2026-09-24 20:03:33 +0700

    Update OrderDetail.php

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -15,7 +15,7 @@ class OrderDetail extends Model
 
     public function food()
     {
-        return $this->belongsTo(Food::class, 'foods_id');
+        return $this->belongsTo(Food::class, 'food_id');
     }
 
     public function order()
