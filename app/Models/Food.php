fb08d7780aa340d4a87e4e4ba86f04a1875e9861
Gir <ahmad.giri195@smk.belajar.id>
2026-09-25 09:30:50 +0700

    Update Food.php
    Comment line

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -7,12 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Food extends Model
 {
-    
     use HasFactory;
 
+    // Menentukan tabel yang digunakan
     protected $table = 'foods';
+
+    // Melindungi kolom id dari mass assignment
     protected $guarded = ['id'];
 
+    // Kolom yang boleh diisi melalui mass assignment
     protected $fillable = [
         'name',
         'category',
