febb65bef59ecf2ba0870415e3f1b634cb06a41c
Gir <ahmad.giri195@smk.belajar.id>
2026-09-25 09:34:14 +0700

    Update Order.php
    comment line

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    /** @use HasFactory<\Database\Factories\OrderFactory> */
+    // Mengaktifkan Factory untuk membuat data dummy
     use HasFactory;
 
+    // Melindungi kolom id dari mass assignment
     protected $guarded = ['id'];
 
+    // Relasi: satu Order memiliki banyak OrderDetail
     public function orderDetails()
     {
         return $this->hasMany(OrderDetail::class, 'order_id');
