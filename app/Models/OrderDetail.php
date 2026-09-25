4748705a3c82b3efa55a2a00e7fcb65e68dbacfa
Gir <ahmad.giri195@smk.belajar.id>
2026-09-25 09:35:18 +0700

    Update OrderDetail.php
    cl

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -7,17 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
-    /** @use HasFactory<\Database\Factories\OrderDetailFactory> */
+    // Mengaktifkan Factory untuk membuat data dummy
     use HasFactory;
 
+    // Menentukan tabel yang digunakan
     protected $table = 'order_details';
+
+    // Melindungi kolom id dari mass assignment
     protected $guarded = ['id'];
 
+    // Relasi: satu OrderDetail dimiliki oleh satu Food
     public function food()
     {
         return $this->belongsTo(Food::class, 'food_id');
     }
 
+    // Relasi: satu OrderDetail dimiliki oleh satu Order
     public function order()
     {
         return $this->belongsTo(Order::class, 'order_id');
